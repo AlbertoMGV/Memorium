932b39abca73641769e8285569d60f37a5c5e743
Alberto Miranda <alberto.miranda@opendeusto.es>
2019-12-05 02:11:30 +0100

    games 5&6 finished

--- MemoriumAPP/src/pages/juego4in/juego4in.ts
+++ MemoriumAPP/src/pages/juego4in/juego4in.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, ToastController } from 'ionic-angular';
+import { MainPage } from '../';
+
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
+
 
 /**
- * Generated class for the Juego4inPage page.
+ * Generated class for the Juego1inPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -13,13 +18,54 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-juego4in',
   templateUrl: 'juego4in.html',
 })
+
 export class Juego4inPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild('slides') slides: Slides;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertController: AlertController, public toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad Juego4inPage');
+    console.log('ionViewDidLoad Juego1inPage');
   }
+
+  terminar() {
+  	this.navCtrl.popToRoot();
+    let toast = this.toastCtrl.create({
+        message: 'Tus resultados se han guardado!!!',
+        duration: 4000,
+        position: 'top'
+      });
+      toast.present();
+  }
+
+  next() {
+    this.slides.slideNext();
+  }
+
+  async back() {
+    const alert = await this.alertController.create({
+      message: '¿Seguro que quieres salir <strong>sin guardar</strong>?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            this.navCtrl.popToRoot();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
 
 }
